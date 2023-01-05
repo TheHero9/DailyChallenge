7da62b61f78181b57386ebc36112bea73c605ba3
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2023-01-05 17:34:56 +0200

    Problem 19 & Problem 20

---
+++ DailyChall/Problem 19 - MissingElement.js
@@ -0,0 +1,29 @@
+//Substract the two sums
+let missingElementArray1 =
+function missingElement2(array1, array2){
+  var add = function(a,b){return a+b}
+
+  return array1.reduce(add) - array2.reduce(add)
+}
+
+
+//Check if the element is missing (only working with unique numbers)
+let missingElementArray2 =
+function missingElement(array1, array2){
+  //For loop
+  for(let i=0; i<array1.length; i++){
+    //Current element
+    let curr = array1[i];
+    //Check if the element is missing
+    if(!array2.includes(curr)){
+      return curr
+    }  
+  }
+}
+
+
+console.log(missingElementArray1([1,2,3,4], [1,4,2,3])) //1
+console.log(missingElementArray1([5,28,3,0], [3,28,5]))//0
+
+console.log(missingElementArray2([1,2,3,4], [1,4,2,3])) //1
+console.log(missingElementArray2([5,28,3,0], [3,28,5]))//0
--- README.md
+++ README.md
@@ -202,4 +202,19 @@ Difficulty: Medium
 
 ## **Problem 19**
 
-### *Combine two Strings*
+### *Missing Element*
+There is an array of non-negative integers. A second array is formed by shuffling the elements of the first array and deleting a random element. Given these two arrays, find which element is missing in the second array.
+
+Difficulty: Easy
+
+
+## **Problem 20**
+
+### *Fibonacci Next Number*
+By definition, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
+
+For example, the first ten Fibonacci numbers are:
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+Write a function that accepts a number and returns the number at that position in the fibonnaci sequence.
+
+Difficulty: Medium
