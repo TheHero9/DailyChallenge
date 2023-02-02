f9599abd5e565250157cfb70327adb89dd42ce37
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2023-02-02 17:07:57 +0200

    Problem 26

---
+++ DailyChall/Js/Problem 26 - MissingNumber.js
@@ -0,0 +1,26 @@
+const MissingNumber = function(array){
+    let result =[]
+
+    // Remove all integers less than 1 and sort the array
+    let filteredSorted = array.filter(e => e>0).sort((a,b) => a-b)
+  
+    filteredSorted.reduce((acc, element) =>{
+      // console.log(`${acc} ${element}`)
+
+    // If the accumulator is different than the element => push into results
+      if(acc !== element){
+        result.push(acc)
+      }
+      return acc+=1
+    }, 1)
+  
+    // Make sure we have a missing number into the array and return the first result
+    if(result[0]){
+      return result[0]
+    }   return "No missing numbers in your array"
+  }
+  
+  
+  console.log(MissingNumber([-1,5,1,2,5,6,7,8])) //3
+  console.log(MissingNumber([-10,-10, 8,90,6,7,8])) //1
+  console.log(MissingNumber([0,0,0,1,2,3,7,6,5,4])) //3
--- README.md
+++ README.md
@@ -246,3 +246,12 @@ the factorial of the given number.
 (A factorial is a function that multiplies a number by every number below it. For example 5!= 5*4*3*2*1=120.)
 
 Difficulty: Easy
+
+
+## **Problem 26**
+### *MissingNumber*
+This problem was asked by Stripe.
+Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0]should give 3.
+You can modify the input array in-place.
